1e6dcbaabcad301aceb4e3d0f37505cd548fd82b
Mike Campbell <mike@exactsolutions.biz>
2017-02-05 17:47:49 -0500

    Added function Get-MaxLengthOfAllAttributes and parameterized some more items with new-UserDSN

--- MigrationDatabase.psm1
+++ MigrationDatabase.psm1
@@ -309,10 +309,16 @@ param
 ,
 [Parameter(Mandatory)]
 [string]$DBServer
+,
+[Parameter()]
+[string]$DriverName = 'SQL Server Native Client 11.0'
+,
+[Parameter()]
+[string]$DriverPath = 'C:\Windows\system32\sqlncli11.dll'
 )
 $RegObjectPath = Join-Path 'HKCU:\SOFTWARE\ODBC\ODBC.INI' $DSNName
 New-Item -Path $RegObjectPath -ItemType Directory
-Set-ItemProperty -Path $RegObjectPath -Name Driver -Value 'C:\Windows\system32\sqlncli11.dll'
+Set-ItemProperty -Path $RegObjectPath -Name Driver -Value $DriverPath
 Set-ItemProperty -Path $RegObjectPath -Name Description -Value $DSNName
 Set-ItemProperty -Path $RegObjectPath -Name Server -Value $DBServer
 Set-ItemProperty -Path $RegObjectPath -Name Trusted_Connection -Value 'Yes'
@@ -322,8 +328,83 @@ $RegObject2Path = 'HKCU:\SOFTWARE\ODBC\ODBC.INI\ODBC Data Sources'
 if (-not (Test-Path -LiteralPath $RegObject2Path)) {
     New-Item -Path $RegObject2Path -ItemType Directory
 }
-Set-ItemProperty -Path $RegObject2Path -Name $DSNName -Value 'SQL Server Native Client 11.0'
+Set-ItemProperty -Path $RegObject2Path -Name $DSNName -Value $DriverName
 }
+function Get-MaxLengthOfAllAttributes
+{
+[cmdletbinding()]
+param(
+[switch]$ShowProgress
+,
+[parameter(ValueFromPipeline)]
+[psobject[]]$InputObject
+)
+begin
+{
+    $AllPropertyMaxLengths = @{}
+    if ($ShowProgress -and $PSBoundParameters.Keys.Contains('InputObject'))
+    {
+        $TotalCount = $InputObject.Count
+        $i = 0
+        $CountKnown = $true
+    }
+    else 
+    {
+        $TotalCount = 0
+        $CountKnown = $false
+    }
+    $WriteProgressParams = @{
+       Activity = 'Analyzing Objects'
+       Status =  "$i of $totalCount"
+       PercentComplete = 0
+    }
+}#begin
+process
+{
+foreach ($o in $InputObject)
+{
+    if ($ShowProgress)
+    {
+        $i++
+        if ($CountKnown -eq $false)
+        {
+            $TotalCount++
+        }
+        $WriteProgressParams.Status = "$i of $totalcount"
+        $WriteProgressParams.PercentComplete = $i/$TotalCount*100
+        Write-Progress  @WriteProgressParams
+    }
+    $OPropertyList = $o | Get-Member -MemberType Properties | Select-object -ExpandProperty Name
+    foreach ($p in $OPropertyList)
+    {
+        if ($o.$p.capacity -ne $null)
+        {
+            $length = @($o.$p.Length) | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+        }
+        else
+        {
+            $length = $o.$p.Length
+        }
+        if ($AllPropertyMaxLengths.ContainsKey($p))
+        {
+            if ($length -gt $AllPropertyMaxLengths.$p)
+            {
+                $AllPropertyMaxLengths.$p = $($length)
+            }
+        }
+        else 
+        {
+            $AllPropertyMaxLengths.$p = $length
+        }
+    }
+}
+}#process
+end
+{
+    $AllPropertyMaxLengths.GetEnumerator() | Sort-object -property Name
+}#end
+}
+
 ##########################################################################################################
 #Initial Database Configuration
 ##########################################################################################################
